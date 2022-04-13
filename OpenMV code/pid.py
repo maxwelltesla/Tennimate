783708e256b8da80ddf69dedc4e875a52c46a587
maxwelltesla <86696381+maxwelltesla@users.noreply.github.com>
2022-04-13 13:37:35 +0800

    Update pid.py

--- OpenMV code/pid.py
+++ OpenMV code/pid.py
@@ -1,28 +1,28 @@
 from pyb import millis
 from math import pi, isnan
  
-class PID:
-    _kp = _ki = _kd = _integrator = _imax = 0
-    _last_error = _last_derivative = _last_t = 0
-    _RC = 1/(2 * pi * 20)
-    def __init__(self, p=0, i=0, d=0, imax=0):
+class PID:   //定义类
+    _kp = _ki = _kd = _integrator = _imax = 0      //定义类变量
+    _last_error = _last_derivative = _last_t = 0   //定义类变量
+    _RC = 1/(2 * pi * 20)                          //定义类变量
+    def __init__(self, p=0, i=0, d=0, imax=0):     //初始化成员变量
         self._kp = float(p)
         self._ki = float(i)
         self._kd = float(d)
         self._imax = abs(imax)
-        self._last_derivative = float('nan')    //'nan'空的使用
+        self._last_derivative = float('nan')       //'nan'空的使用
  
-    def get_pid(self, error, scaler):
+    def get_pid(self, error, scaler):              //定义成员函数（不是类函数）
         tnow = millis()
         dt = tnow - self._last_t
         output = 0
-        if self._last_t == 0 or dt > 1000:   //时间跨度为零：刚开始。时间跨度>1000，累计时长
+        if self._last_t == 0 or dt > 1000:         //时间跨度为零：刚开始。时间跨度>1000，累计时长
             dt = 0
-            self.reset_I()    //重置积分项
+            self.reset_I()                         //重置积分项
         self._last_t = tnow
         delta_time = float(dt) / float(1000)
-        output += error * self._kp        //按比例值累计误差
-        if abs(self._kd) > 0 and dt > 0:  //有时差和累计微分项
+        output += error * self._kp                 //按比例值累计误差
+        if abs(self._kd) > 0 and dt > 0:           //有时差和累计微分项
             if isnan(self._last_derivative):
                 derivative = 0
                 self._last_derivative = 0
@@ -35,12 +35,12 @@ class PID:
             self._last_derivative = derivative
             output += self._kd * derivative
         output *= scaler
-        if abs(self._ki) > 0 and dt > 0: //有时差和累计积分项
+        if abs(self._ki) > 0 and dt > 0:            //有时差和累计积分项
             self._integrator += (error * self._ki) * scaler * delta_time
             if self._integrator < -self._imax: self._integrator = -self._imax
             elif self._integrator > self._imax: self._integrator = self._imax
             output += self._integrator
         return output
-    def reset_I(self):
+    def reset_I(self):                              //定义成员函数（不是类函数）
         self._integrator = 0
         self._last_derivative = float('nan')
